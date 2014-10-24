225f9bbb46ddd79dc5bc2e5ea9748df6b4ca4af4
darkrocks <dm.kazinov@gmail.com>
2014-10-24 20:20:40 +0400

    presentation-viewer

--- app/components/presentation-viewer/presentation-viewer-directive.js
+++ app/components/presentation-viewer/presentation-viewer-directive.js
@@ -2,20 +2,35 @@
 'use strict';
 
 angular.module('presenter.presentation-viewer.presentation-viewer-directive', [])
-	.directive('presentationViewer', [function () {
+.constant('controlPanelHeight', 50)
+.constant('aspectRatio', 1.3333)
+	.directive('presentationViewer', ['controlPanelHeight', 'aspectRatio', function (controlPanelHeight, aspectRatio) {
+
 		return {
 			restrict: 'E',
 			templateUrl: 'components/presentation-viewer/presentation-viewer-directive.html',
 			scope: {
-				presentation: '='
+			  presentation: '=',
+			  viewerHeight: '='
 			},
 			link: function (scope, element, attrs) {
 				scope.currentSlideNumber = 1;
 				var $svg = $(element).find('svg');
-			  
-				scope.$watch("presentation", function (n, o) {
+
+				function setSlide() {
+				  var $slideContent = $(scope.presentation.slides[scope.currentSlideNumber - 1]);
+				  var slideHeight = scope.viewerHeight - controlPanelHeight;
+				  var slideWidth = slideHeight * aspectRatio;
+				  $svg.attr('height', slideHeight);
+				  $svg.attr('width', slideWidth);
+				  
+				  $svg.html($slideContent.html());
+			    
+			  }
+
+			  scope.$watch("presentation", function (n, o) {
 				  if (scope.presentation && scope.presentation.slides && scope.currentSlideNumber <= scope.presentation.slides.length && scope.currentSlideNumber >= 1) {
-				    $svg.html(scope.presentation.slides[scope.currentSlideNumber - 1]);
+				    setSlide();
 				  }
 				}, true);
 
@@ -30,7 +45,7 @@ angular.module('presenter.presentation-viewer.presentation-viewer-directive', []
 					throw Error('Incurrect current slide number');
 				}
 
-				$svg.html(scope.presentation.slides[scope.currentSlideNumber - 1]);
+				setSlide();
 			}
 		};
 	}]);
