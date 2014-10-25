01bf7723222ba2cee0cafb1813359e68349b06d3
darkrocks <dm.kazinov@gmail.com>
2014-10-25 15:18:42 +0400

    presentation-viewer

--- app/components/presentation-viewer/presentation-viewer-directive.js
+++ app/components/presentation-viewer/presentation-viewer-directive.js
@@ -2,50 +2,87 @@
 'use strict';
 
 angular.module('presenter.presentation-viewer.presentation-viewer-directive', [])
-.constant('controlPanelHeight', 50)
-.constant('aspectRatio', 1.3333)
-	.directive('presentationViewer', ['controlPanelHeight', 'aspectRatio', function (controlPanelHeight, aspectRatio) {
+.constant('horisontalIndent', 300)
+.constant('virticalIndent', 130)
+.constant('svgAspectRatio', 1.7774)
 
-		return {
-			restrict: 'E',
-			templateUrl: 'components/presentation-viewer/presentation-viewer-directive.html',
-			scope: {
-			  presentation: '=',
-			  viewerHeight: '='
-			},
-			link: function (scope, element, attrs) {
-				scope.currentSlideNumber = 1;
-				var $svg = $(element).find('svg');
+	.directive('presentationViewer', ['horisontalIndent', 'virticalIndent', 'svgAspectRatio', function (horisontalIndent, virticalIndent, svgAspectRatio) {
 
-				function setSlide() {
-				  var $slideContent = $(scope.presentation.slides[scope.currentSlideNumber - 1]);
-				  var slideHeight = scope.viewerHeight - controlPanelHeight;
-				  var slideWidth = slideHeight * aspectRatio;
-				  $svg.attr('height', slideHeight);
-				  $svg.attr('width', slideWidth);
-				  
-				  $svg.html($slideContent.html());
-			    
-			  }
+	  return {
+	    restrict: 'E',
+	    templateUrl: 'components/presentation-viewer/presentation-viewer-directive.html',
+	    scope: {
+	      presentation: '=',
+	      viewerHeight: '='
+	    },
+	    link: function (scope, element, attrs) {
+	      scope.currentSlideNumber = 1;
+	      var $svg = $(element).find('svg');
 
-			  scope.$watch("presentation", function (n, o) {
-				  if (scope.presentation && scope.presentation.slides && scope.currentSlideNumber <= scope.presentation.slides.length && scope.currentSlideNumber >= 1) {
-				    setSlide();
-				  }
-				}, true);
+	      scope.nextSlide = function() {
+	        if (scope.currentSlideNumber + 1 > scope.presentation.slides.length) {
+	          return;
+	        }
 
-				if (!scope.presentation) {
-					return;
-				}
-				if (!scope.presentation || !scope.presentation.slides || !scope.presentation.slides.count) {
-					throw Error('Wrong presentation format');
-				}
+	        scope.currentSlideNumber += 1;
+	        setSlide();
+	      };
 
-				if (scope.currentSlideNumber > scope.presentation.slides.length || scope.currentSlideNumber < 1) {
-					throw Error('Incurrect current slide number');
-				}
+	      scope.prevSlide = function () {
+	        if (scope.currentSlideNumber - 1 < 1) {
+	          return;
+	        }
 
-				setSlide();
-			}
-		};
+	        scope.currentSlideNumber -= 1;
+	        setSlide();
+	      };
+
+
+	      fitSvgSize();
+	      $(window).resize(fitSvgSize);
+
+	      scope.$watch("presentation", function (n, o) {
+	        if (scope.presentation && scope.presentation.slides && scope.currentSlideNumber <= scope.presentation.slides.length && scope.currentSlideNumber >= 1) {
+	          setSlide();
+	        }
+	      }, true);
+
+	      if (!scope.presentation) {
+	        return;
+	      }
+	      if (!scope.presentation || !scope.presentation.slides || !scope.presentation.slides.count) {
+	        throw Error('Wrong presentation format');
+	      }
+
+	      if (scope.currentSlideNumber > scope.presentation.slides.length || scope.currentSlideNumber < 1) {
+	        throw Error('Incurrect current slide number');
+	      }
+
+	      setSlide();
+
+
+	      function fitSvgSize() {
+	        var maxWidth = $(window).width() - horisontalIndent;
+	        var maxHeight = $(window).height() - virticalIndent;
+
+	        var svgWidth = maxWidth;
+	        var svgHeight = maxHeight;
+	        if (maxWidth / maxHeight > svgAspectRatio) {
+	          svgWidth = maxHeight * svgAspectRatio;
+	        } else {
+	          svgHeight = maxWidth / svgAspectRatio;
+	        }
+
+	        $svg.attr('height', svgHeight);
+	        $svg.attr('width', svgWidth);
+	      }
+
+	      function setSlide() {
+	        fitSvgSize();
+	        var $slideContent = $(scope.presentation.slides[scope.currentSlideNumber - 1]);
+	        $svg.html($slideContent.html());
+	      }
+
+	    }
+	  };
 	}]);
